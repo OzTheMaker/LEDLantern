aa115dd475b1a379b8243c68003e89d9e5992be3
OzTheMAker <megaman80045@gmail.com>
2023-12-06 19:57:38 -0600

    Single LED stream routine

--- LEDLantern.X/LEDLantern.s
+++ LEDLantern.X/LEDLantern.s
@@ -2,7 +2,16 @@ PROCESSOR 10LF322
 #include<xc.inc> ;Includes macros defined by PIC-as
 
 ;Definitions
-CLOCK_16 EQU 0x70    
+CLOCK_16    EQU 0x70
+R_BLUE	    EQU 0x40
+BLUE	    EQU 0	;Blue color value    
+R_RED	    EQU 0x41
+RED	    EQU	255	;Red color value
+R_GREEN	    EQU 0x42
+GREEN	    EQU	0	;Green color value
+COUNTER_B   EQU 0x43
+COUNTER_R   EQU 0x44
+COUNTER_G   EQU 0x45 
     
 ;Configurating word
 CONFIG WDTE = OFF   ;WATCHDOG OFF
@@ -26,13 +35,45 @@ GPIO_SETUP:
     MOVWF TRISA ;RA0 pin = OUTPUT
     
 MAIN:
-    BSF	PORTA, PORTA_RA0_POSITION    ;LED1 ON
-    BCF PORTA, PORTA_RA0_POSITION    ;LED1 OFF
-    GOTO MAIN	
+    MOVLW 8
+    MOVWF COUNTER_B
+    MOVWF COUNTER_R
+    MOVWF COUNTER_G
+    MOVLW BLUE
+    MOVWF R_BLUE
+    MOVLW RED
+    MOVWF R_RED
+    MOVLW GREEN
+    MOVWF R_GREEN
+
+BLUE_OUT:  
+    BCF PORTA, PORTA_RA0_POSITION ;Send a 0 (resets the stream)
+    BSF PORTA, PORTA_RA0_POSITION ;Send a 1 (starts the stream)
+    BTFSS R_BLUE, 0 ;Checks the bit to send 
+    BCF PORTA, PORTA_RA0_POSITION ;If 0 sends a 0
+    RRF R_BLUE, F ;Rotate the register to the right
+    DECFSZ COUNTER_B, F ;Decrement the counter for R_BLUE
+    GOTO BLUE_OUT ;If it is not 0 send the next bit
+    GOTO RED_OUT ; Else go to next stream 
     
-    
-    
-    
-    
-    
-    
+
+RED_OUT:
+    BCF PORTA, PORTA_RA0_POSITION ;Send a 0 (resets the stream)
+    BSF PORTA, PORTA_RA0_POSITION ;Send a 1 (starts the stream)
+    BTFSS R_RED, 0 ;Checks the bit to send 
+    BCF PORTA, PORTA_RA0_POSITION ;If 0 sends a 0
+    RRF R_RED, F ;Rotate the register to the right
+    DECFSZ COUNTER_R, F ;Decrement the counter for R_BLUE
+    GOTO RED_OUT ;If it is not 0 send the next bit
+    GOTO GREEN_OUT ; Else go to next stream 
+
+GREEN_OUT:
+    BCF PORTA, PORTA_RA0_POSITION ;Send a 0 (resets the stream)
+    BSF PORTA, PORTA_RA0_POSITION ;Send a 1 (starts the stream)
+    BTFSS R_GREEN, 0 ;Checks the bit to send 
+    BCF PORTA, PORTA_RA0_POSITION ;If 0 sends a 0
+    RRF R_GREEN, F ;Rotate the register to the right
+    DECFSZ COUNTER_G, F ;Decrement the counter for R_GREEN
+    GOTO GREEN_OUT ;If it is not 0 send the next bit
+            
+END ;Turns ON a LED
