c429f853f653248ffd0890545deded0105b420e9
OzTheMAker <megaman80045@gmail.com>
2023-12-08 23:45:12 -0600

    Clock stabilization routine added

--- LEDLantern.X/LEDLantern.s
+++ LEDLantern.X/LEDLantern.s
@@ -4,17 +4,20 @@ PROCESSOR 10LF322
 ;Definitions
 CLOCK_16    EQU 0x70
 R_BLUE	    EQU 0x40
-BLUE	    EQU 0	;Blue color value    
 R_RED	    EQU 0x41
-RED	    EQU	255	;Red color value
 R_GREEN	    EQU 0x42
-GREEN	    EQU	0	;Green color value
 COUNTER_B   EQU 0x43
 COUNTER_R   EQU 0x44
 COUNTER_G   EQU 0x45
 LED_ARRAY   EQU 0x46   
-LED_COUNTER EQU 2	;Number of LEDs to control   
-    
+  
+
+;Variables 
+BLUE	    EQU 0	;Blue color value  
+RED	    EQU	255	;Red color value
+GREEN	    EQU	0	;Green color value	    
+LED_COUNTER EQU 2	;Number of LEDs to control 
+ 
 ;Configurating word
 CONFIG WDTE = OFF   ;WATCHDOG OFF
 CONFIG CP = OFF	    ;CODE PROTECTION OFF
@@ -31,7 +34,10 @@ RESET_VECTOR:
 CLOCK_SOURCE_CONFIG:
     MOVLW CLOCK_16  ;INTERNAL CLOCK FREQUENCY = 16 MHz 
     MOVWF OSCCON
-    
+
+PRIMARY_CLOCK_DELAY:
+    BTFSS OSCCON, 0 ;Stops the program till the oscillator become stable
+    GOTO PRIMARY_CLOCK_DELAY
     
 SETUP:
     MOVLW ~(1 << PORTA_RA0_POSITION)
@@ -57,29 +63,29 @@ LOOP:
 LED_STREAM:
     
     GREEN_OUT:
-	BCF PORTA, PORTA_RA0_POSITION ;Send a 0 (resets the stream)
 	BSF PORTA, PORTA_RA0_POSITION ;Send a 1 (starts the stream)
 	BTFSS R_GREEN, 0 ;Checks the bit to send 
 	BCF PORTA, PORTA_RA0_POSITION ;If 0 sends a 0
 	RRF R_GREEN, F ;Rotate the register to the right
+	BCF PORTA, PORTA_RA0_POSITION ;Send a 0 (resets the stream)
 	DECFSZ COUNTER_G, F ;Decrement the counter for R_GREEN
 	GOTO GREEN_OUT ;If it is not 0 send the next bit    
     
     RED_OUT:
-	BCF PORTA, PORTA_RA0_POSITION ;Send a 0 (resets the stream)
 	BSF PORTA, PORTA_RA0_POSITION ;Send a 1 (starts the stream)
 	BTFSS R_RED, 0 ;Checks the bit to send 
 	BCF PORTA, PORTA_RA0_POSITION ;If 0 sends a 0
 	RRF R_RED, F ;Rotate the register to the right
+	BCF PORTA, PORTA_RA0_POSITION ;Send a 0 (resets the stream)
 	DECFSZ COUNTER_R, F ;Decrement the counter for R_BLUE
 	GOTO RED_OUT ;If it is not 0 send the next bit
 
     BLUE_OUT:  
-	BCF PORTA, PORTA_RA0_POSITION ;Send a 0 (resets the stream)
 	BSF PORTA, PORTA_RA0_POSITION ;Send a 1 (starts the stream)
 	BTFSS R_BLUE, 0 ;Checks the bit to send 
 	BCF PORTA, PORTA_RA0_POSITION ;If 0 sends a 0
 	RRF R_BLUE, F ;Rotate the register to the right
+	BCF PORTA, PORTA_RA0_POSITION ;Send a 0 (resets the stream)
 	DECFSZ COUNTER_B, F ;Decrement the counter for R_BLUE
 	GOTO BLUE_OUT ;If it is not 0 send the next bit
 	BCF PORTA, PORTA_RA0_POSITION ;Reset the stream 
